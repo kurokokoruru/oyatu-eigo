62619cd846d4b09e6a96d9694faeb118e45fc8e7
青木孝之 <kuroko@aokitakayukinoMac-mini.local>
2025-07-10 19:33:39 +0900

    add files

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: [],
+};
